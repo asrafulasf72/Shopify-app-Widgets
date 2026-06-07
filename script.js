39a8856697046fa71e14a474b7122bfe8c79fa5e
Asraful <asrafulasraful166@gmail.com>
2026-06-07 14:55:47 +0600

    List panel open added

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const sortDropdown = document.querySelector('.qr-sort-dropdown');
 const sortBtn = document.querySelector('.qr-sort-btn');
 const sortItems = document.querySelectorAll('.qr-sort-menu li');
 
+sortBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  sortDropdown.classList.toggle('is-open');
+});
+
