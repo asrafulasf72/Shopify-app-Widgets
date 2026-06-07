eec01acb60a45ab05224f87d2e6f41c6062b7cb9
Asraful <asrafulasraful166@gmail.com>
2026-06-07 14:54:49 +0600

    Dom delected

---
+++ script.js
@@ -0,0 +1,4 @@
+const sortDropdown = document.querySelector('.qr-sort-dropdown');
+const sortBtn = document.querySelector('.qr-sort-btn');
+const sortItems = document.querySelectorAll('.qr-sort-menu li');
+
