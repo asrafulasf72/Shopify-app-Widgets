4b48526ad2f77e6c13d930570247c37ea2ac0700
Asraful <asrafulasraful166@gmail.com>
2026-06-07 14:58:58 +0600

    session storage Dom Selected

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 const sortDropdown = document.querySelector('.qr-sort-dropdown');
 const sortBtn = document.querySelector('.qr-sort-btn');
+const sortBtnText = document.querySelector('.qr-sort-btn-text');
 const sortItems = document.querySelectorAll('.qr-sort-menu li');
 
-sortBtn.addEventListener('click', (e) => {
-  e.stopPropagation();
-  sortDropdown.classList.toggle('is-open');
-});
-document.addEventListener('click', () => {
-  sortDropdown.classList.remove('is-open');
-});
-sortItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    
-    sortItems.forEach((li) => {
-      li.classList.remove('active');
-    });
-
-    item.classList.add('active');
-
-  });
-});
+const savedSort = sessionStorage.getItem('selectedSort');
 
