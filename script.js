c85694345d2049799334c965c184399019636355
Asraful <asrafulasraful166@gmail.com>
2026-06-07 15:00:21 +0600

    Data set sessionStorage

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const sortItems = document.querySelectorAll('.qr-sort-menu li');
 
 const savedSort = sessionStorage.getItem('selectedSort');
 
+if (savedSort) {
+  sortItems.forEach((item) => {
+    if (item.dataset.sort === savedSort) {
+      item.classList.add('active');
+      sortBtnText.textContent = `Sort by: ${item.textContent.trim()}`;
+    }
+  });
+}
+
